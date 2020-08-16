344e857e1fcf0e491df5b8cac345a37c0f55c89c
Leonardo Ferreira <lleoalves02@gmail.com>
2020-08-16 13:22:17 -0300

    Adicionado arquivo para o runcodes

--- questão2_e_runcodes.py
+++ questão2_e_runcodes.py
@@ -1,12 +1,23 @@
-altura = int(input())
-comprimento = int(input())
-largura = int(input())
+def piso_sala(lar, com):
+    return lar * com
 
-area_do_piso_da_sala = largura * comprimento
-volume_da_sala = largura * comprimento * altura
-area_das_paredes_da_sala = 2 * altura * largura + 2 * altura * comprimento
+def volume_sala(lar, com, altu):
+    return lar * com * altu
 
-print(f"{area_do_piso_da_sala:.2f}/{volume_da_sala:.2f}/{area_das_paredes_da_sala:.2f}")
+def parede_sala(lar, com, altu):
+    return (2 * altu * lar) + (2 * altu * com)
 
+def main():
+    altura = float(input())
+    comprimento = float(input())
+    largura = float(input())
 
+    area_do_piso_da_sala = piso_sala(largura, comprimento)
+    volume_da_sala = volume_sala(largura, comprimento, altura)
+    area_das_paredes_da_sala = parede_sala(largura, comprimento, altura)
+
+    print(f"{area_do_piso_da_sala:.2f} metros, {volume_da_sala:.2f} {area_das_paredes_da_sala:.2f}")
+
+if __name__ == '__main__':
+    main()
 
