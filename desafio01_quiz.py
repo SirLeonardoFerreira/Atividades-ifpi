ff5a5b494e2b2fcce093eaeae057e103d0702630
Leonardo Ferreira <lleoalves02@gmail.com>
2020-08-30 20:36:36 -0300

    Feito os desafios da atividade quiz

---
+++ desafio01_quiz.py
@@ -0,0 +1,16 @@
+def main():
+    print("Qual foi o time campeão da libertadores de 2019?")
+    resultado = input()
+
+    if resultado.upper() == 'FLAMENGO':
+        print("Certa resposta :)")
+
+    else:
+        print("Errou :(")
+
+    print("Obrigado por jogar!")
+
+if __name__ == '__main__':
+    main()
+
+    
