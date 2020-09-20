f704e3166ede130db6c1365bbc8e4949530a0105
SirLeonardoFerreira <lleoalves02@gmail.com>
2020-09-20 19:59:55 -0300

    removido arquivos

--- questão3_semana5_atividade02.py
+++
@@ -1,9 +0,0 @@
-def main():
-    for bug in range(99, 251, 7):
-        print(f'{bug} bugs no software, pegue sete deles e conserte...')
-        print(f'Tecle "Ctrl+F5"')
-    print(f'Vamos fazer mais um café!')
-
-    
-if __name__ == '__main__':
-    main()
